56eb19c5eb3474cc3510ccb839b89dd87f9ff6f3
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-19 18:03:55 +0300

    update gsap

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,6 +1,5 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import getIsDesk from './getIsDesk';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -8,19 +7,16 @@ const tl = gsap.timeline();
 const mm = gsap.matchMedia();
 
 mm.add('(min-width: 1440px)', () => {
-  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 0);
-  tl.fromTo('.about-text-wrap', { x: '0%' }, { x: '-590px', duration: 1 }, 0);
-  tl.fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 0 }, 0.5);
-  tl.fromTo('.about-slider-how-it-works-wrap.how-it-works', { opacity: 1 }, { opacity: 0, duration: 0 }, 0.5);
-  tl.fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 0 }, 0.5);
-  tl.fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 0 }, 0.5);
+  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 0.5)
+    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 0 }, 1)
+    .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 0 }, 1);
 
   ScrollTrigger.create({
     animation: tl,
     trigger: '.about-content-wrap',
     start: 'center center',
-    end: 'bottom',
-    scrub: true,
+    end: 'bottom 50%',
     pin: true,
+    toggleActions: 'play none play reverse',
   });
 });
