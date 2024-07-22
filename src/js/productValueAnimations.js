d4f88a2bb3d7f2bf5d181f8fedc3ee65e57f73b4
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-22 11:07:11 +0300

    fix animations

--- src/js/productValueAnimations.js
+++ src/js/productValueAnimations.js
@@ -1,1 +1,15 @@
-console.log(1);
+import refs from './refs';
+
+refs.productValueCard.forEach((card) => {
+  const animation = card.querySelector('lottie-player');
+
+  card.addEventListener('mouseenter', () => {
+    animation.setDirection(1);
+    animation.play();
+  });
+
+  card.addEventListener('mouseleave', () => {
+    animation.setDirection(-1);
+    animation.play();
+  });
+});
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,7 @@ const refs = {
   contactsUsForm: document.querySelector('.js-contacts-us-form'),
   contactsUsFormSubmitBtn: document.querySelector('.js-contacts-us-form-submit-btn'),
   phoneInput: document.querySelector('.js-contacts-us-form-phone-input'),
+  productValueCard: document.querySelectorAll('.js-product-value-card'),
 };
 
 export default refs;
