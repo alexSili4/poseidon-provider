dfc77fdc65fab58e4256809cdd3e8a3a6a9d70bd
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-22 16:57:17 +0300

    fix

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -7,8 +7,7 @@ const tl = gsap.timeline();
 const mm = gsap.matchMedia();
 
 mm.add('(min-width: 1440px)', () => {
-  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 0.5)
-    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 0 }, 1)
+  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 1.5)
     .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 1)
     .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1);
 
@@ -16,7 +15,22 @@ mm.add('(min-width: 1440px)', () => {
     animation: tl,
     trigger: '.about-content-wrap',
     start: 'center center',
-    end: 'bottom 50%',
+    end: 'bottom',
+    pin: true,
+    toggleActions: 'play none play reverse',
+  });
+});
+
+mm.add('(max-width: 1439px)', () => {
+  tl.fromTo('.about-slider-bg', { y: '0%' }, { y: '-302px', duration: 1 }, 1.5)
+    .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 1)
+    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1);
+
+  ScrollTrigger.create({
+    animation: tl,
+    trigger: '.about-content-wrap',
+    start: 'center center',
+    end: 'bottom',
     pin: true,
     toggleActions: 'play none play reverse',
   });
