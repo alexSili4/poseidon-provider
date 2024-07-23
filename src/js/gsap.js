e9c5d3d5c0c931f5a5962ea52ceef3cc4d5cac6e
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-23 15:35:31 +0300

    Update gsap.js

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -3,37 +3,40 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline();
 const mm = gsap.matchMedia();
 
 mm.add('(min-width: 1440px)', () => {
-  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 1.5)
-    .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 1)
-    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1)
-    .fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 0 }, 1.5);
+  const tl = gsap.timeline();
+
+  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 0)
+    .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 0)
+    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 0)
+    .fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 1 }, 0);
 
   ScrollTrigger.create({
     animation: tl,
     trigger: '.about-content-wrap',
     start: 'center center',
-    end: 'bottom',
+    end: 'center',
     pin: true,
-    toggleActions: 'play none play reverse',
+    toggleActions: 'play none none reverse',
   });
 });
 
 mm.add('(max-width: 1439px)', () => {
-  tl.fromTo('.about-slider-bg', { y: '0%' }, { y: '-396px', duration: 1 }, 1.5)
-    .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 1)
-    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1)
-    .fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 0 }, 1.5);
+  const tl = gsap.timeline();
+
+  tl.fromTo('.about-slider-bg', { y: '0%' }, { y: '-396px', duration: 1 }, 0)
+    .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 0)
+    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 0)
+    .fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 1 }, 0);
 
   ScrollTrigger.create({
     animation: tl,
     trigger: '.about-content-wrap',
     start: 'center center',
-    end: 'bottom',
+    end: 'center+=60',
     pin: true,
-    toggleActions: 'play none play reverse',
+    toggleActions: 'play none none reverse',
   });
 });
