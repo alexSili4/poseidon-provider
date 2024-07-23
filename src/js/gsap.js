af62e5dfdde129a202657e21ca06fe6b8c1de7a8
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-23 12:56:07 +0300

    fix

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -7,9 +7,10 @@ const tl = gsap.timeline();
 const mm = gsap.matchMedia();
 
 mm.add('(min-width: 1440px)', () => {
-  tl.fromTo('.about-slider-bg-wrap', { x: '0%' }, { x: '610px', duration: 1 }, 1.5)
+  tl.fromTo('.about-slider-bg', { x: '0%' }, { x: '610px', duration: 1 }, 1.5)
     .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 1)
-    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1);
+    .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1)
+    .fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 0 }, 1.5);
 
   ScrollTrigger.create({
     animation: tl,
@@ -22,10 +23,10 @@ mm.add('(min-width: 1440px)', () => {
 });
 
 mm.add('(max-width: 1439px)', () => {
-  tl.fromTo('.about-slider-bg-wrap', { y: '0%' }, { y: '-396px', duration: 1 }, 1.5)
+  tl.fromTo('.about-slider-bg', { y: '0%' }, { y: '-396px', duration: 1 }, 1.5)
     .fromTo('.about-interaction-scheme-animation', { opacity: 0 }, { opacity: 1, duration: 1 }, 1)
     .fromTo('.about-slider-wrap', { opacity: 1 }, { opacity: 0, duration: 1 }, 1)
-    .fromTo('.about-slider-bg-wrap', { 'background-color': '#ffffff' }, { 'background-color': 'transparent', duration: 0 }, 1.7);
+    .fromTo('.about-slider-how-it-works-wrap.interaction-scheme', { opacity: 0 }, { opacity: 1, duration: 0 }, 1.5);
 
   ScrollTrigger.create({
     animation: tl,
